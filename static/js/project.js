aa190da8997ddf6c933744ac4aee3b58131aa83b
HaoyunWu <wuhaoyu1@msu.edu>
2023-06-12 22:36:17 -0400

    Added scroll animation

--- static/js/project.js
+++ static/js/project.js
@@ -1,14 +1,36 @@
-const boxes=document.querySelectorAll('.projectbox');
 
-window.addEventListener('scroll',()=>{
-    const triggle=window.innerHeight*0.8;
-    boxes.forEach((item)=>{
-        const boxTop=item.getBoundingClientRect().top;
+function getElementViewTop(element){
+    var actualTop = element.offsetTop;
+    var current = element.offsetParent;
+    
+    while (current !== null){
+    actualTop += current. offsetTop;
+    current = current.offsetParent;
+    }
+    
+    if (document.compatMode == "BackCompat"){
+        var elementScrollTop=document.body.scrollTop;
+    } else {
+        var elementScrollTop=document.documentElement.scrollTop;
+    }
+    
+    return actualTop-elementScrollTop;
+    }
 
-        if(boxTop<triggle){
-            boxTop.classList.add('show')
-        }else{
-            boxTop.classList.remove('show')
-        }
-    })
+document.addEventListener('scroll', (e) => {
+    const rows = document.querySelectorAll("div.project")
+    const html = document.documentElement
+
+    for (let [index, row] of rows.entries()) {
+
+        const Top =  getElementViewTop(row)
+        let scrolled = 1.5 - Top / html.clientHeight
+        let progress = scrolled
+        if (progress >= 1) progress = 1
+        if (progress <= 0) progress = 0
+        
+        row.style.setProperty('--progress', progress)
+    }
 })
+
+
