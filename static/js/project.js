92b79d01b776808d6174848e46737304bdeed502
HaoyunWu <wuhaoyu1@msu.edu>
2023-06-13 00:17:56 -0400

    Added introduce animation

--- static/js/project.js
+++ static/js/project.js
@@ -18,7 +18,7 @@ function getElementViewTop(element){
     }
 
 document.addEventListener('scroll', (e) => {
-    const rows = document.querySelectorAll("div.project")
+    const rows = document.querySelectorAll("div.container")
     const html = document.documentElement
 
     for (let [index, row] of rows.entries()) {
@@ -33,4 +33,19 @@ document.addEventListener('scroll', (e) => {
     }
 })
 
+document.addEventListener('scroll', (e) => {
+    const rows = document.querySelectorAll("div.project")
+    const html = document.documentElement
+
+    for (let [index, row] of rows.entries()) {
+
+        const Top =  getElementViewTop(row)
+        let scrolled = 1.5 - Top / html.clientHeight
+        let progress = scrolled
+        if (progress >= 1) progress = 1
+        if (progress <= 0) progress = 0
+        
+        row.style.setProperty('--progress', progress)
+    }
+})
 
